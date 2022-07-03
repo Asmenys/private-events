91895886a4c2fba4fc338b6c0c2cd4b5ec2e3fca
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-03 10:45:41 +0000

    added new validation for event.body, created an event display partial _event, created an event show page that displays information about a single event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,8 @@ class Event < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
     validates :body, length: {minimum: 10}
+    validates :body, length: {maximum: 150}
     validates :event_date, presence: true
     validates :location, presence: true
+    
 end
